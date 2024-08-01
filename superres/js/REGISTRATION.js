d3b28572c2d32beb8e5b1877f5d56416e9e61734
DeadWolfX <jonathanjairsc@gmail.com>
2024-07-31 19:06:27 -0600

    final version of the project and deployed webpage

--- superres/js/REGISTRATION.js
+++ superres/js/REGISTRATION.js
@@ -1,3 +1,3 @@
 document.getElementById("registerButton").onclick = function() {
-    //window.open("http://localhost/superres/REGISTRATION.html", '_blank');
+    window.open("https://docs.google.com/forms/d/1CK14XgfB86rEwjsZtlMwFKdrJ27MSlxg1L_3e8Tp4a0/viewform?edit_requested=true", '_blank');
 };
